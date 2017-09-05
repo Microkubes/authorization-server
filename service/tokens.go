b595535274834936c37ad73303dd08a5a6de5b61
Pavle Jonoski <pavle.jonoski@keitaro.com>
2017-09-05 17:52:29 +0200

    Checking for token espiration.

--- service/tokens.go
+++ service/tokens.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"time"
+
 	"github.com/JormungandrK/authorization-server/db"
 	"github.com/JormungandrK/microservice-security/oauth2"
 )
@@ -15,9 +17,29 @@ func (t *OAuth2TokenService) SaveToken(token oauth2.OAuth2Token) error {
 }
 
 func (t *OAuth2TokenService) GetToken(refreshToken string) (*oauth2.OAuth2Token, error) {
-	return t.TokenRepository.GetForRefreshToken(refreshToken)
+	token, err := t.TokenRepository.GetForRefreshToken(refreshToken)
+	if err != nil {
+		return nil, err
+	}
+	if token == nil || isExpired(token) {
+		return nil, nil
+	}
+	return token, nil
 }
 
 func (t *OAuth2TokenService) GetTokenForClient(userID, clientID string) (*oauth2.OAuth2Token, error) {
-	return t.TokenRepository.GetForClientAndUser(clientID, userID)
+	token, err := t.TokenRepository.GetForClientAndUser(clientID, userID)
+	if err != nil {
+		return nil, err
+	}
+	if token == nil || isExpired(token) {
+		return nil, nil
+	}
+	return token, nil
+}
+
+func isExpired(token *oauth2.OAuth2Token) bool {
+	now := time.Now()
+	tokenValidUntil := time.Unix(0, token.IssuedAt).Add(time.Duration(token.ValidFor) * time.Millisecond)
+	return now.After(tokenValidUntil)
 }
